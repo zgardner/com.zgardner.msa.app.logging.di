af437e01bc40e48930dd63b50b37a10a60bdd5e6
Zach Gardner <zgardner@keyholesoftware.com>
2015-08-22 14:04:42 -0500

    Not worrying about a post build for now.

--- postBuild.sh
+++
@@ -1,6 +0,0 @@
-git config --global user.name "Jenkins"
-git config --global user.email "jenkin@com.zgardner.msa.app.logging.di"
-
-git add com.zgardner.msa.app.logging.api.war
-git commit -m "Bundling Logging API war for build #${BUILD_NUMBER}"
-git push
